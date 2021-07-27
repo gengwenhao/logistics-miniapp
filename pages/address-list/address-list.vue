00062cef72ef0fd237c6960081fa835328ab6233
gengwenhao <gengwenhao97@126.com>
2021-07-27 17:50:07 +0800

    完成地址簿页面

---
+++ pages/address-list/address-list.vue
@@ -0,0 +1,156 @@
+<template>
+  <view id="address-list">
+    <!-- 控件组 -->
+    <view class="control-group">
+      <view class="control">
+        <view class="btn">新增地址</view>
+      </view>
+      <view class="control">
+        <u-input v-model="form.keyWord" height="60" :type="type" :border="true" placeholder="请输入姓名/手机号/地址"/>
+      </view>
+    </view>
+
+    <view class="address-group">
+      <view class="address" v-for="item in 9" :key="item">
+        <view class="information">
+          <view class="name-line">
+            <view class="name">陈东东</view>
+            <view class="tel">13299999555</view>
+          </view>
+          <div class="place-line">
+            福建省福州市鼓楼区华林路155号新华兴联合广场A座20层XXXXXXXXXX
+          </div>
+        </view>
+        <view class="edit-group">
+          <view class="inner-con">
+            <view class="edit-item">编辑</view>
+            <view class="edit-item">删除</view>
+          </view>
+        </view>
+      </view>
+      <view class="more">
+        <u-loadmore icon-type="flower" :status="loadStatus" :load-text="loadText"/>
+      </view>
+    </view>
+
+
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 加载状态
+      loadStatus: null,
+      loadText: {
+        loadmore: '轻轻上拉',
+        loading: '努力加载中',
+        nomore: '没有更多数据了'
+      },
+      form: {
+        keyWord: ''
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped lang="scss">
+#address-list {
+  min-height: 100vh;
+  height: 100%;
+  background: #f2f2f2;
+
+  // 控件组
+  .control-group {
+    background: #FFFFFF;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 23rpx;
+
+    // 按钮
+    .control:nth-child(1) {
+      flex: 1;
+      height: 64rpx;
+      line-height: 64rpx;
+      margin-right: 20rpx;
+
+      .btn {
+        background: #02a7f0;
+        color: #FFFFFF;
+        text-align: center;
+        border: 2rpx solid #EFEFEF;
+        border-radius: 50rpx;
+        font-size: 28rpx;
+      }
+    }
+
+    // 输入框
+    .control:nth-child(2) {
+      flex: 2;
+    }
+  }
+
+  // 地址列表
+  .address-group {
+    padding: 22rpx;
+
+    .address {
+       border-radius: 18rpx;
+      background: #FFFFFF;
+      margin-bottom: 24rpx;
+      padding: 12rpx 18rpx;
+
+      // 信息
+      .information {
+        border-bottom: 2rpx solid #f2f2f2;
+        padding-bottom: 24rpx;
+        padding-top: 24rpx;
+
+        .name-line {
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-start;
+          align-items: center;
+          font-size: 32rpx;
+          color: #333333;
+
+          .tel {
+            text-indent: 46rpx;
+          }
+
+        }
+
+        .place-line {
+          color: #8f8d8e;
+          padding-top: 8rpx;
+        }
+      }
+
+      // 编辑
+      .edit-group{
+        padding: 16rpx 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-end;
+        color: #8f8d8e;
+
+        .inner-con{
+          display: flex;
+          flex-direction: row;
+          justify-content: flex-start;
+          align-items: center;
+
+          .edit-item{
+            margin-right: 26rpx;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/userprofile/userprofile.vue
+++ pages/userprofile/userprofile.vue
@@ -1,22 +1,106 @@
 <template>
-	<view>
-		
-	</view>
+  <view id="userprofile">
+    <u-row gutter="16">
+
+      <!-- 顶部 -->
+      <view class="avatar-panel">
+        <u-image width="150rpx" height="150rpx" src="../../../static/images/avatar.png" shape="circle"/>
+        <view class="nickname">哆唻A梦</view>
+      </view>
+
+      <!-- 功能组 -->
+      <view class="tool-list">
+        <view class="tool">
+          <u-image width="60rpx" height="60rpx" src="../../../static/images/地址簿图标.png"/>
+          <view class="tool-title" @click="toAddressList">地址簿</view>
+        </view>
+      </view>
+
+      <!-- 退出登录 -->
+      <view class="logout-panel">
+        <view class="btn">退出登录</view>
+      </view>
+
+    </u-row>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    toAddressList() {
+      uni.navigateTo({
+        url: '../address-list/address-list'
+      })
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped lang="scss">
+#userprofile {
+  background: rgba(242, 242, 242, 1);
+  min-height: 100vh;
+  height: 100%;
 
+  // 头像面板
+  .avatar-panel {
+    background: #FFFFFF;
+    height: 180rpx;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 80rpx;
+
+    .nickname {
+      text-indent: 38rpx;
+      font-size: 35rpx;
+    }
+  }
+
+  // 功能组
+  .tool-list {
+    padding: 30rpx 20rpx;
+
+    .tool {
+      padding-left: 40rpx;
+      background: #FFFFFF;
+      height: 110rpx;
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: center;
+      border-radius: 20rpx;
+      font-size: 30rpx;
+      color: #333333;
+
+      .tool-title {
+        margin-left: 18rpx;
+      }
+    }
+  }
+
+  // 退出登录
+  .logout-panel {
+    width: 100vw;
+    position: fixed;
+    bottom: 150rpx;
+
+    .btn {
+      width: 45vw;
+      height: 95rpx;
+      line-height: 95rpx;
+      background: #FFFFFF;
+      font-size: 34rpx;
+      border: 2rpx solid #939393;
+      border-radius: 12rpx;
+      margin: 0 auto;
+      text-align: center;
+    }
+  }
+}
 </style>
